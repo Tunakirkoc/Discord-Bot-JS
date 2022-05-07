dcf33167847c45de124f7816766007d00c0c0b0b
Tuna Kirkoc <tuna@kirkoc.fr>
2022-05-08 01:07:37 +0200

    yes

--- events/tag-league of legends.js
+++ events/tag-league of legends.js
@@ -4,8 +4,9 @@ module.exports = {
         console.log(newMember.activities);
         if (newMember.activities[0] !== undefined) {
             if (newMember.activities[0].name.toLowerCase() == 'league of legends') {
-                if (oldMember.activities[0].name.toLowerCase() != 'league of legends') {
+                if (oldMember.activities[0] == undefined || oldMember.activities[0].name.toLowerCase() != 'league of legends') {
                     newMember.guild.channels.cache.find(channel => channel.name === 'boo').send(newMember.user.toString() + "\nhttps://tenor.com/view/touching-grass-touch-gif-21219969")
+                    console.log(newMember.user.name + ' is playing LoL');
                 }
             }
         }
