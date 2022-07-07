7a0752432704ff8c11997d8382fb6e8a23d7b1e3
Tuna Kirkoc <tuna@kirkoc.fr>
2022-07-07 08:35:13 +0200

    Yes

--- events/join-to-create.js
+++ events/join-to-create.js
@@ -6,7 +6,7 @@ module.exports = {
     event: 'voiceStateUpdate',
     execute(oldState, newState) {
         if (newState.channelId === joinToCreate.channelId) {
-            if (newState.channel.members.size === 1) {
+            if (newState.channel.members.size >= 1) {
                 const member = newState.channel.members.entries().next().value[1]
                 newState.guild.channels.create(`${member.displayName}` + " channel", {
                     type: 'GUILD_VOICE',
